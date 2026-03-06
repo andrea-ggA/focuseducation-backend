6f1e7ff814c610a3c53876a944717f6a2b45ffec
andrea-ggA <andreapietra123@gmail.com>
2026-03-06 11:57:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -47,6 +47,8 @@ app.post("/generate-quiz", async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server avviato su http://localhost:3000");
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
